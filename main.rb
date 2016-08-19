5a06795ae6f13932574104d5d22c3abab72e2153
tomoyuki.tanaka <tomoyuki.tanaka@rakus.co.jp>
2016-08-19 05:25:31 +0000

    add sessions.erb

--- main.rb
+++ main.rb
@@ -13,10 +13,16 @@ disable :protection
 enable :sessions
 
 DB = DbConnector.new
+$sessions = []
 
+#show index
 get '/' do
+  unless session[:name]
+    redirect to '/question'
+  end
+
   session[:value] ||= rand 100
-  @cookie = session[:value]
+  @cookie = session
   @title = 'main'
   @users = []
   users = DB.con.query("SELECT * FROM users")
@@ -25,9 +31,14 @@ get '/' do
     @users << User.new(user)
   end
 
+  unless $sessions.any? { |s| s[:session_id] == session[:session_id] }
+    $sessions << session
+  end
+
   erb :index
 end
 
+#return users json data
 post '/users' do
   content_type :json
   result = DB.con.query("SELECT * FROM users WHERE id = #{params[:id]}")
@@ -41,24 +52,33 @@ post '/users' do
   output.to_json
 end
 
+#show user create form
 get '/new' do
   @title = 'new'
   @content = 'new_content'
   erb :new
 end
 
+#user create
 post '/create' do
   DB.con.query("INSERT INTO users (name, age) VALUES ('#{params['name']}','#{params['age']}')")
   redirect to '/'
 end
 
+#show question form
 get '/question' do
   erb :question
 end
 
+#create session by question
 post '/question' do
   session[:name], session[:join], session[:tec], session[:assign] = params['name'], params['joined_day'], params['tec'], params['assign']
   session[:news], session[:person], session[:reason] = params['news'], params['person'], params['reason']
+  session[:cookie] = env['HTTP_COOKIE']
+  unless $sessions.any? { |s| s[:session_id] == session[:session_id] }
+    $sessions << session
+  end
+
   redirect to '/thank'
 end
 
@@ -66,9 +86,15 @@ get '/thank' do
   erb :thank
 end
 
+get '/sessions' do
+  @sessions = $sessions
+  erb :sessions
+end
+
+#helper for views
 helpers do
   def link_to(url, txt = url)
-    %Q(<a href="#{url}">"#{txt}"</a>)
+    %Q(<a href="#{url}">#{txt}</a>)
   end
 end
 
